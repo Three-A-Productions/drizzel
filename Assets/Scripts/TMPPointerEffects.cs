03819cdc9fec05e02fae92ebc36fcd042356009e
v81d <v81d@proton.me>
2026-03-04 16:05:54 -0500

    feat: add hover and active states to menu buttons

---
+++ Assets/Scripts/TMPPointerEffects.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+using TMPro;
+
+public class TMPPointerEffects : MonoBehaviour,
+    IPointerEnterHandler,
+    IPointerExitHandler,
+    IPointerDownHandler,
+    IPointerUpHandler
+{
+    [SerializeField] private Color hoverColor;
+    [SerializeField] private Color activeColor;
+
+    private TMP_Text tmpText;
+    private Color startingColor;
+    private bool isHovering = false;
+
+    void Awake()
+    {
+        tmpText = GetComponent<TMP_Text>();
+        startingColor = tmpText.color;
+    }
+
+    public void OnPointerEnter(PointerEventData _) // hovering over text
+    {
+        isHovering = true;
+        tmpText.color = hoverColor;
+    }
+
+    public void OnPointerExit(PointerEventData _) // not hovering over text anymore
+    {
+        isHovering = false;
+        tmpText.color = startingColor;
+    }
+
+    public void OnPointerDown(PointerEventData _) // text is active
+    {
+        tmpText.color = activeColor;
+    }
+
+    public void OnPointerUp(PointerEventData _) // text is not active anymore
+    {
+        tmpText.color = isHovering ? hoverColor : startingColor;
+    }
+}
