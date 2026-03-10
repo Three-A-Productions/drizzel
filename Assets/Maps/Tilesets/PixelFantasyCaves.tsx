45d7e32a7e11b21c85b230a3fff017c4154f4761
Aaron Wijesinghe <aaronw1975@icloud.com>
2026-03-10 19:07:51 -0400

    Update tilesets with upscaled images

--- Assets/Maps/Tilesets/PixelFantasyCaves.tsx
+++ Assets/Maps/Tilesets/PixelFantasyCaves.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.2" name="PixelFantasyCaves" tilewidth="16" tileheight="16" tilecount="4096" columns="64">
- <image source="../../Sprites/MapSprites/PixelFantasyCaves/PixelFantasyCaves.png" width="1024" height="1024"/>
+<tileset version="1.10" tiledversion="1.11.2" name="PixelFantasyCaves" tilewidth="32" tileheight="32" tilecount="4096" columns="64">
+ <image source="../../Sprites/MapSprites/PixelFantasyCaves/PixelFantasyCaves.png" width="2048" height="2048"/>
 </tileset>
