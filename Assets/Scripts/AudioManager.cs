965a059314a9d15359ebba32757349000ac847ac
v81d <v81d@proton.me>
2026-03-11 02:32:56 +0000

    feat(main-menu): play music in menu

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -1,19 +1,12 @@
 using System.Collections;
 using UnityEngine;
 
-[System.Serializable]
-public class BackgroundLoop
-{
-    public AudioClip audioClip;
-    public float volume;
-}
-
 public class AudioManager : MonoBehaviour
 {
     public static AudioManager Instance { get; private set; }
 
     [SerializeField]
-    private BackgroundLoop[] backgroundLoops;
+    private BackgroundTrack[] backgroundLoops;
 
     private void Awake()
     {
@@ -23,30 +16,88 @@ public class AudioManager : MonoBehaviour
             return;
         }
         Instance = this;
+    }
 
-        foreach (BackgroundLoop loop in backgroundLoops)
+    private void Start()
+    {
+        foreach (BackgroundTrack loop in backgroundLoops)
             PlayAudio(loop.audioClip, loop.volume, true);
     }
 
-    public void PlayAudio(AudioClip audioClip, float volume = 1f, bool loop = false)
+    public void PlayAudio(
+        AudioClip audioClip,
+        float volume = 1f,
+        bool loop = false,
+        bool fadeIn = false,
+        bool fadeOut = false,
+        float fadeTime = 0f
+    )
     {
-        StartCoroutine(PlayCoroutine(audioClip, volume, loop));
+        StartCoroutine(PlayCoroutine(audioClip, volume, loop, fadeIn, fadeOut, fadeTime));
     }
 
-    private IEnumerator PlayCoroutine(AudioClip audioClip, float volume = 1f, bool loop = false)
+    private IEnumerator PlayCoroutine(
+        AudioClip audioClip,
+        float volume = 1f,
+        bool loop = false,
+        bool fadeIn = false,
+        bool fadeOut = false,
+        float fadeTime = 0f
+    )
     {
         AudioSource audioSource = gameObject.AddComponent<AudioSource>();
         audioSource.clip = audioClip;
         audioSource.volume = volume;
         audioSource.loop = loop;
-        audioSource.Play();
+
+        if (fadeIn)
+            StartCoroutine(FadeInAudio(audioSource, fadeTime, volume));
+        else
+            audioSource.Play();
 
         if (!loop)
         {
-            yield return new WaitForSeconds(audioSource.clip.length);
+            if (fadeOut && fadeTime > 0f)
+            {
+                float waitTime = Mathf.Max(0, audioSource.clip.length - fadeTime);
+                yield return new WaitForSeconds(waitTime);
+                yield return StartCoroutine(FadeOutAudio(audioSource, fadeTime));
+            }
+            else
+                yield return new WaitForSeconds(audioSource.clip.length);
+
             Destroy(audioSource);
         }
-        else
-            yield break;
+    }
+
+    private IEnumerator FadeInAudio(AudioSource audioSource, float fadeTime, float targetVolume)
+    {
+        audioSource.Play();
+        float timer = 0f;
+
+        while (timer < fadeTime)
+        {
+            timer += Time.deltaTime;
+            audioSource.volume = Mathf.Lerp(0f, targetVolume, timer / fadeTime);
+            yield return null;
+        }
+
+        audioSource.volume = targetVolume;
+    }
+
+    private IEnumerator FadeOutAudio(AudioSource audioSource, float fadeTime)
+    {
+        float startVolume = audioSource.volume;
+        float timer = 0f;
+
+        while (timer < fadeTime)
+        {
+            timer += Time.deltaTime;
+            audioSource.volume = Mathf.Lerp(startVolume, 0f, timer / fadeTime);
+            yield return null;
+        }
+
+        audioSource.volume = 0f;
+        audioSource.Stop();
     }
 }
--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,16 +1,51 @@
+using System.Collections;
 using UnityEngine;
 
 public class MainMenu : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public static MainMenu Instance { get; private set; }
+
+    [SerializeField]
+    private BackgroundTrack[] backgroundTracks;
+
+    private bool isPlayingTrack = false;
+
+    private void Awake()
     {
-        
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        Instance = this;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        StartCoroutine(RandomMusicLoop());
+    }
+
+    private IEnumerator RandomMusicLoop()
+    {
+        while (true)
+        {
+            if (!isPlayingTrack)
+                PlayRandomSong();
+
+            yield return null;
+        }
+    }
+
+    private void PlayRandomSong()
+    {
+        if (isPlayingTrack)
+            return;
+
+        isPlayingTrack = true;
+
+        int idx = Random.Range(0, backgroundTracks.Length);
+        BackgroundTrack track = backgroundTracks[idx];
+
+        AudioManager.Instance.PlayAudio(track.audioClip, track.volume, false, true, true, 15f);
     }
 }
